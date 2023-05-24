8d635f88e34ada7dbf1a8e1681957f5e9083ddae
GustavoBalsamo <gubalsamo@hotmail.com>
2023-05-24 20:49:32 -0300

    header criado, com responsividade funcionando

---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,35 @@
+import logo from '../../assets/logo.svg'
+import hamburger from '../../assets/BurgenBtn.svg'
+import styles from './index.module.css'
+import {Menu,MenuButton,MenuList,MenuItem,} from '@chakra-ui/react'
+
+export const Header = () => {
+    return (
+        <section className={styles.header}>
+            <img className={styles.img} src={logo}/>
+            <div className={styles.navbar_links}>
+                <a href="#" className={styles.navbar_mark} >Home</a>
+                <a href="#">Pokédex</a>
+                <a href="#">Legendaries</a>
+                <a href="#">Documentation</a>
+            </div>
+            <div className={styles.hamburger}>
+                <Menu>
+                    <MenuButton >
+                        <img src={hamburger} />
+                    </MenuButton>
+                    <MenuList>
+                        <MenuItem ><a href="#"><div>Home</div></a>
+                        </MenuItem>
+                        <MenuItem ><a href="#"><div>Pokedéx</div></a>
+                        </MenuItem>
+                        <MenuItem ><a href="#"><div>Legendaries</div></a>
+                        </MenuItem>
+                        <MenuItem ><a href="#"><div>Documentation</div></a>
+                        </MenuItem>
+                    </MenuList>
+                </Menu>
+            </div>
+        </section>
+    )
+}
