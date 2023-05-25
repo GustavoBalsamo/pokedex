88b7ae7065782b7b3d45a8a4b7f0735523dc0015
GustavoBalsamo <gubalsamo@hotmail.com>
2023-05-24 21:10:13 -0300

    home feita no html

---
+++ src/Components/Home/index.tsx
@@ -0,0 +1,21 @@
+import pikachu from '../../assets/banner.png'
+import pikachu_mobile from '../../assets/BannerComplete.png'
+import { Header } from '../Header'
+import styles from './index.module.css'
+
+export const Home = () => {
+    return (
+        <div>
+    <Header/>
+            <section className={styles.section}>
+                <img className={styles.pikachu} src={pikachu_mobile}/>
+                <div className={styles.description}>
+                    <h1 className={styles.title}><strong>Find</strong> all your favorite <strong>Pokemon</strong></h1>
+                    <h2 className={styles.information}>You can know the type of Pokemon, its strengths, disadvantages and abilities</h2>
+                    <button className={styles.button}>See pokemons</button>
+                </div>
+                <img className={styles.image} src={pikachu}/>
+            </section>
+        </div>
+    )
+}
